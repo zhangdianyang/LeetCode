58a07ad0cd43f3f3cfa2d3127434813c00b79220
dianyangz <dyzhang@trip.com>
2021-02-23 11:59:28 +0800

    LinkedListCycle

--- src/main/java/leetcode/editor/cn/LinkedListCycle.java
+++ src/main/java/leetcode/editor/cn/LinkedListCycle.java
@@ -58,6 +58,12 @@ public class LinkedListCycle {
 
     public static void main(String[] args) {
         Solution solution = new LinkedListCycle().new Solution();
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+        solution.hasCycle(node1);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -84,8 +90,12 @@ public class LinkedListCycle {
         private boolean hasCycleOne(ListNode head) {
             ListNode fast = head;
             ListNode slow = head;
-            while (fast != null) {
-                
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) {
+                    return true;
+                }
             }
             return false;
         }
